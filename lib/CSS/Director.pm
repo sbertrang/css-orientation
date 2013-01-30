76cc152fbd75b846a2f945408aa81799af8a1d58
Simon Bertrang <janus@errornet.de>
2013-01-30 23:40:05 +0100

    pod changes

--- lib/CSS/Director.pm
+++ lib/CSS/Director.pm
@@ -601,7 +601,7 @@ __END__
 
 =head1 NAME
 
-CSS::Director - Perl extension for blah blah blah
+CSS::Director - Perl extension to change orientation in CSS
 
 =head1 SYNOPSIS
 
@@ -623,14 +623,7 @@ None by default.
 
 =head1 SEE ALSO
 
-Mention other useful documentation such as the documentation of
-related modules or operating system documentation (such as man pages
-in UNIX), or any relevant external documentation such as RFCs or
-standards.
-
-If you have a mailing list set up for your module, mention it here.
-
-If you have a web site set up for your module, mention it here.
+L<http://code.google.com/p/cssjanus/>
 
 =head1 AUTHOR
 
@@ -643,7 +636,6 @@ Copyright (C) 2013 by A. U. Thor
 This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself, either Perl version 5.12.2 or,
 at your option, any later version of Perl 5 you may have available.
-
 
 =cut
 
